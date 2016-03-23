95ad8400f1332708c24491314146fcd5a42d9b94
Ron Masas <ronm@bbinary.com>
2016-03-23 16:08:15 +0200

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -5,18 +5,20 @@
   Description: Protect your WordPress login page from robots.
   Version: 1.0.0
  */
-
-// Make sure Shortcut is active
-if (!class_exists('Shortcut')) {
-    return;
-}
-
-/**
- * @ver array
- */
-$files = array(
-    'controllers/*.php',
-    'app.php'
-);
-
-Shortcut::make(__FILE__)->requireAll($files);
+ 
+ add_action('plugins_loaded',function(){
+    // Make sure Shortcut is active
+    if (!class_exists('Shortcut')) {
+        return;
+    }
+    
+    /**
+     * @ver array
+     */
+    $files = array(
+        'controllers/*.php',
+        'app.php'
+    );
+    
+    Shortcut::make(__FILE__)->requireAll($files);
+ });
